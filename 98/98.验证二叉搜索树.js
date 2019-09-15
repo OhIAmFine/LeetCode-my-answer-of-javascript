aae83e2370db09c8f565b0ce4e4b52a9c9ac313b
OhIAmFine <845782625@qq.com>
2019-09-15 11:27:19 +0800

    add 98th

---
+++ 98/98.验证二叉搜索树.js
@@ -0,0 +1,17 @@
+// 中序遍历
+
+var isValidBST = function(root) {
+  const stack = []
+  let inorder = -Infinity
+  while(stack.length || root != null){
+    while(root != null) {
+      stack.push(root)
+      root = root.left
+    }
+    root = stack.pop()
+    if(root.val <= inorder) return false
+    inorder = root.val
+    root = root.right
+  }
+  return true
+};
