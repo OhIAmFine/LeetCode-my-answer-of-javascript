6c7c76fb447c835d10d66d1e229049089228b598
OhIAmFine <845782625@qq.com>
2019-09-16 11:33:13 +0800

    add 236th

---
+++ 236/236.二叉树的最近公共祖先.js
@@ -0,0 +1,8 @@
+// 采用递归的方式
+
+var lowestCommonAncestor = function(root, p, q) {
+  if(!root || root === p || root === q) return root
+  const left = lowestCommonAncestor(root.left, p, q)
+  const right = lowestCommonAncestor(root.right, p, q)
+  return !left ? right : (!right ? left : root)
+};
