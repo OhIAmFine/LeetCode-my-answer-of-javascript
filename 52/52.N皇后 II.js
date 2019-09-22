1e594fc9ac111cebbb1d10b7df6e4339db26c279
OhIAmFine <845782625@qq.com>
2019-09-22 22:52:24 +0800

    add second 52th

--- 52/52.N皇后 II.js
+++ 52/52.N皇后 II.js
@@ -1,26 +1,45 @@
-var totalNQueens = function(n) {
-  const res = []
-  const col = new Set()
-  const pie = new Set()
-  const na = new Set()
-  const solveQueen = (level, curState) => {
+// var totalNQueens = function(n) {
+//   const res = []
+//   const col = new Set()
+//   const pie = new Set()
+//   const na = new Set()
+//   const solveQueen = (level, curState) => {
+//     if (level >= n) {
+//       res.push(curState)
+//     }
+//     for (let i = 0; i < n; i++) {
+//       if (col.has(i) || pie.has(level + i) || na.has(level - i)) {
+//         continue
+//       }
+//       col.add(i)
+//       pie.add(level + i)
+//       na.add(level - i)
+//       solveQueen(level + 1, [...curState, i])
+//       col.delete(i)
+//       pie.delete(level + i)
+//       na.delete(level - i)
+//     }
+//   }
+//   solveQueen(0, [])
+//   return res.length
+// };
+
+// 第二种解法
+var totalNQueens = function (n) {
+  let count = 0
+  const DFS = (level, col, pie, na) => {
     if (level >= n) {
-      res.push(curState)
+      count++
+      return
     }
-    for (let i = 0; i < n; i++) {
-      if (col.has(i) || pie.has(level + i) || na.has(level - i)) {
-        continue
-      }
-      col.add(i)
-      pie.add(level + i)
-      na.add(level - i)
-      solveQueen(level + 1, [...curState, i])
-      col.delete(i)
-      pie.delete(level + i)
-      na.delete(level - i)
+    let baits = (~(col | pie | na)) & ((1 << n) - 1)
+    while (baits > 0) {
+      const p = baits & -baits
+      DFS(level + 1, col | p, (pie | p) << 1, (na | p) >> 1)
+      baits &= baits - 1
     }
   }
-  solveQueen(0, [])
-  return res.length
-};
+  DFS(0, 0, 0, 0)
+  return count
+}
 console.log(totalNQueens(4))
