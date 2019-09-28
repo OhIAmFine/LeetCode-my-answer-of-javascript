6df2020d69d077f3f0c68048709e30eca9f9dc81
OhIAmFine <845782625@qq.com>
2019-09-28 11:28:53 +0800

    refactor code

--- 152/152.乘积最大子序列.js
+++ 152/152.乘积最大子序列.js
@@ -1,16 +1,4 @@
-var maxProduct = function(nums) {
-  let res = []
-  let max = 1
-  for(let i = 0; i < nums.length; i ++) {
-    if(nums[i] * max <= nums[i]) {
-      res.push(nums[i] * max)
-      max = nums[i]
-    }else {
-      max = nums[i] * max
-    }
+var maxProduct = function (nums) {
+}
 
-  }
-  return Math.max(...res)
-};
-
-console.log(maxProduct([2,3,-2,4]))
+console.log(maxProduct([2, 3, -2, 4]))
