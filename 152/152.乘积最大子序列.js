067c56d10a2ae1732717ca7c31a460ff199d42e6
OhIAmFine <845782625@qq.com>
2019-09-30 01:01:46 +0800

    add 152th

--- 152/152.乘积最大子序列.js
+++ 152/152.乘积最大子序列.js
@@ -1,4 +1,26 @@
 var maxProduct = function (nums) {
+  if (!nums.length) return 0
+  if (nums.length === 1) return nums[0]
+  let dp = new Array(nums.length).fill(1).map(() => [[1][0]])
+  if (nums[0] >= 0) {
+    dp[0][0] = nums[0]
+    dp[0][1] = 0
+  } else {
+    dp[0][0] = 0
+    dp[0][1] = nums[0]
+  }
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] >= 0) {
+      dp[i][0] = Math.max(dp[i - 1][0] * nums[i], nums[i])
+      dp[i][1] = Math.min(dp[i - 1][1] * nums[i], nums[i])
+    } else {
+      dp[i][0] = Math.max(dp[i - 1][1] * nums[i], nums[i])
+      dp[i][1] = Math.min(dp[i - 1][0] * nums[i], nums[i])
+    }
+  }
+
+  return Math.max(...dp.map((arr) => arr[0]))
 }
 
-console.log(maxProduct([2, 3, -2, 4]))
+console.log(maxProduct([-2]))
