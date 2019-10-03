e933af41d5c2e42fb98075a795a146d0138d8c24
OhIAmFine <845782625@qq.com>
2019-10-03 23:48:53 +0800

    refactor code

--- 188/188.买卖股票的最佳时机 IV.js
+++ 188/188.买卖股票的最佳时机 IV.js
@@ -9,23 +9,18 @@ var maxProfit = function (k, prices) {
       MP[i][j][1] = -Infinity
     }
   }
-
   MP[0][0][0] = prices[0]
   MP[0][0][1] = -Infinity
   for (let i = 1; i < k; i++) {
     MP[0][i][0] = prices[0]
     MP[0][i][1] = -Infinity
   }
-  console.log(MP)
-
-
   for (let i = 1; i < prices.length; i++) {
     for (let j = 1; j < k; j++) {
       MP[i][j][0] = Math.max(MP[i - 1][j][0], MP[i - 1][j - 1][1] + prices[i])
       MP[i][j][1] = Math.max(MP[i - 1][j][1], MP[i - 1][j - 1][0] - prices[i])
     }
   }
-  console.log(MP)
 }
 
 maxProfit(2, [3,2,6,5,0,3])
