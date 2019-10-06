e27759b0d387e3758b4b009dff0e550dfd07c7c1
OhIAmFine <845782625@qq.com>
2019-10-06 21:18:54 +0800

    add 188th

--- 188/188.买卖股票的最佳时机 IV.js
+++ 188/188.买卖股票的最佳时机 IV.js
@@ -1,28 +1,38 @@
 var maxProfit = function (k, prices) {
-  const MP = []
+  var maxProfit = function (k, prices) {
+    if (!k) return 0
 
-  for (let i = 0; i < prices.length; i++) {
-    MP[i] = new Array()
-    for (let j = 0; j < k; j++) {
-      MP[i][j] = new Array()
-      MP[i][j][0] = -Infinity
-      MP[i][j][1] = -Infinity
+    // 相当于不限次数，能赚钱的都加上
+    if (k >= prices.length - 1) {
+      let max = 0
+      for (let i = 1; i < prices.length; i++) {
+        let v = prices[i] - prices[i - 1]
+        if (v > 0) max += v
+      }
+      return max
     }
-  }
-  MP[0][0][0] = prices[0]
-  MP[0][0][1] = -Infinity
-  debugger
-  for (let i = 1; i < k; i++) {
-    MP[0][i][0] = prices[0]
-    MP[0][i][1] = -Infinity
-  }
-  for (let i = 1; i < prices.length; i++) {
-    for (let j = 0; j < k; j++) {
-      MP[i][j][0] = Math.max(MP[i - 1][j][0], MP[i - 1][j - 1][1] + prices[i])
-      MP[i][j][1] = Math.max(MP[i - 1][j][1], MP[i - 1][j][0] - prices[i])
+
+    // 限制次数
+    let nBuyNSell = new Array(k).fill(0)
+    let nBuy = new Array(k).fill(Number.POSITIVE_INFINITY)
+
+    for (let i = 0; i < prices.length; i++) {
+      const p = prices[i]
+
+      // 购买价格
+      nBuy[0] = Math.min(nBuy[0], p)
+      // 出售后的利润
+      nBuyNSell[0] = Math.max(nBuyNSell[0], p - nBuy[0])
+
+      for (let j = 1; j < k; j++) {
+        // 第N次购买的投入，综合了前一次的利润
+        nBuy[j] = Math.min(nBuy[j], p - nBuyNSell[j - 1])
+        // 第N次出售的利润，因为算投入的时候综合了前一次的利润，所以这就是总利润
+        nBuyNSell[j] = Math.max(nBuyNSell[j], p - nBuy[j])
+      }
     }
+    return nBuyNSell[nBuyNSell.length - 1]
   }
-  debugger
 }
 
-maxProfit(2, [3,2,6,5,0,3])
+maxProfit(2, [3, 2, 6, 5, 0, 3])
