b5f5a963e9853a4c3832e9cebdf4adac2f5c47ab
OhIAmFine <845782625@qq.com>
2019-10-07 21:50:08 +0800

    add 322th

---
+++ 322/322.零钱兑换.js
@@ -0,0 +1,14 @@
+var coinChange = function (coins, amount) {
+  let max = amount + 1
+  let DP = new Array(max).fill(Infinity)
+  DP[0] = 0
+  for (let i = 1; i <= amount; i++) {
+    for (let j = 0; j < coins.length; j++) {
+      if (coins[j] <= i) {
+        DP[i] = Math.min(DP[i], DP[i - coins[j]] + 1)
+      }
+    }
+  }
+  return DP[amount] > amount ? -1 : DP[amount]
+}
+console.log(coinChange([1, 2, 5], 11))
