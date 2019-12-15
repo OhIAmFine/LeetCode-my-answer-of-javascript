6110fc50c8318328c0a23438d8d89d2bdbe14fb3
OhIAmFine <845782625@qq.com>
2019-12-15 23:51:19 +0800

    add 242th

---
+++ 242/242. 有效的字母异位词.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+var isAnagram = function(s, t) {
+  var obj = {}
+  for(let i = 0; i < s.length; i ++){
+    if(obj[s[i]]){
+      obj[s[i]] += 1
+    }else {
+      obj[s[i]] = 1
+    }
+  }
+  for(let i = 0; i < t.length; i ++){
+    if(obj[t[i]] > 0){
+      obj[t[i]] -= 1
+      continue
+    }
+    if(obj[t[i]] === 0){
+      delete obj[t[i]]
+      continue
+    }
+    if(!obj[t[i]]) return false
+  }
+  return !Number(Object.values(obj).join(''))
+}
