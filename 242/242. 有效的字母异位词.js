a07d255d0513820fcae7703e4c7c6f49f30b2db1
OhIAmFine <845782625@qq.com>
2019-12-15 23:52:06 +0800

    refactor 242th

--- 242/242. 有效的字母异位词.js
+++ 242/242. 有效的字母异位词.js
@@ -4,25 +4,25 @@
  * @return {boolean}
  */
 
-var isAnagram = function(s, t) {
+var isAnagram = function (s, t) {
   var obj = {}
-  for(let i = 0; i < s.length; i ++){
-    if(obj[s[i]]){
+  for (let i = 0; i < s.length; i++) {
+    if (obj[s[i]]) {
       obj[s[i]] += 1
-    }else {
+    } else {
       obj[s[i]] = 1
     }
   }
-  for(let i = 0; i < t.length; i ++){
-    if(obj[t[i]] > 0){
+  for (let i = 0; i < t.length; i++) {
+    if (obj[t[i]] > 0) {
       obj[t[i]] -= 1
       continue
     }
-    if(obj[t[i]] === 0){
+    if (obj[t[i]] === 0) {
       delete obj[t[i]]
       continue
     }
-    if(!obj[t[i]]) return false
+    if (!obj[t[i]]) return false
   }
   return !Number(Object.values(obj).join(''))
 }
