2e8eeee7a6ff82845236cc18e2028fb6693b2bf6
OhIAmFine <845782625@qq.com>
2020-02-27 22:03:20 +0800

    add  455th

--- 455/455. 分发饼干.js
+++ 455/455. 分发饼干.js
@@ -4,5 +4,22 @@
  * @return {number}
  */
 var findContentChildren = function(g, s) {
+    g.sort((a, b) => a - b);
+    s.sort((a, b) => a - b);
 
+    let gi = 0;
+    let sj = 0;
+    let res = 0;
+
+    while (gi < g.length && sj < s.length) {
+        if (g[gi] <= s[sj]) {
+            gi++;
+            sj++;
+            res++;
+        } else {
+            sj++;
+        }
+    }
+    return res;
 };
+
