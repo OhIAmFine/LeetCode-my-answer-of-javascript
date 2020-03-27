ca2a5a1acf70175233ca32ee31bdcb91f174b09a
OhIAmFine <845782625@qq.com>
2020-03-27 20:46:32 +0800

    refactor 507th

--- 507/507. 完美数.js
+++ 507/507. 完美数.js
@@ -2,22 +2,22 @@
  * @param {number} num
  * @return {boolean}
  */
-var checkPerfectNumber = function(num) {
-  if(num == 1) {
-    return false;
+var checkPerfectNumber = function (num) {
+  if (num == 1) {
+    return false
   }
-  let sum = 1; // 正整数一定会有一个1，同时不用考虑自身，所以单独处理
-  let i = 2;
-  const sqrt = Math.sqrt(num);
-  for(;i < sqrt;i++) {
-    if(num % i == 0) {
-      sum += i;
-      sum += num / i;
+  let sum = 1 // 正整数一定会有一个1，同时不用考虑自身，所以单独处理
+  let i = 2
+  const sqrt = Math.sqrt(num)
+  for (; i < sqrt; i++) {
+    if (num % i == 0) {
+      sum += i
+      sum += num / i
     }
   }
   // 此处单独处理的原因在于只需要加1次i值，如果在循环中会加2次
-  if(i * i == num) {
-    sum += i;
+  if (i * i == num) {
+    sum += i
   }
-  return sum == num;
-};
+  return sum == num
+}
