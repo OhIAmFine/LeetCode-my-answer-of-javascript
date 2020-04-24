59bf9dd3993fc3352b7c411d8ccf89874759fdde
OhIAmFine <845782625@qq.com>
2020-04-24 18:19:07 +0800

    add  563th

---
+++ 563/563. 二叉树的坡度.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var findTilt = function(root) {
+    if(!root)return 0;
+    return Math.abs(dfs(root.left)-dfs(root.right))+findTilt(root.left)+findTilt(root.right);
+
+    function dfs(root){
+        if(!root)return 0;
+        return root.val + dfs(root.left)+dfs(root.right);
+    }
+};
