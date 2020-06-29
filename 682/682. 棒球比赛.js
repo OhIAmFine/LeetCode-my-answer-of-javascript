67cb82728edbcd5d580432427789b49359c1eecf
OhIAmFine <845782625@qq.com>
2020-06-29 22:41:51 +0800

    refactor  682th

--- 682/682. 棒球比赛.js
+++ 682/682. 棒球比赛.js
@@ -1,29 +1,29 @@
-var calPoints = function(ops) {
-  let arr = [];
+var calPoints = function (ops) {
+  let arr = []
   let opera = {
-    "+": function() {
-      let total = arr.length > 1 ? Number(arr[arr.length - 1]) + Number(arr[arr.length -2]): arr[0];
-      arr.push(total);
+    '+': function () {
+      let total = arr.length > 1 ? Number(arr[arr.length - 1]) + Number(arr[arr.length - 2]) : arr[0]
+      arr.push(total)
     },
-    "D": function() {
+    'D': function () {
       arr.length && arr.push(arr[arr.length - 1] * 2)
     },
-    "C": function() {
-      arr.pop();
+    'C': function () {
+      arr.pop()
     },
-    'in': function(item) {
-      arr.push(item);
+    'in': function (item) {
+      arr.push(item)
     }
-  };
+  }
 
   ops.forEach((item) => {
     if (item in opera) {
-      opera[item]();
+      opera[item]()
     } else {
-      opera.in(item);
+      opera.in(item)
     }
   })
 
-  return arr.reduce((total, num) =>  Number(total) + Number(num))
+  return arr.reduce((total, num) => Number(total) + Number(num))
 
-};
+}
