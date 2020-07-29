5678ed5de306281620fbcf994ec599047814685a
OhIAmFine <845782625@qq.com>
2020-07-29 18:58:26 +0800

    add  18th

---
+++ 18/18. 四数之和.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function (nums, target) {
+    console.log(nums.length < 4)
+
+    if (nums.length < 4) return []
+    const res = []
+    let left = 2
+    let right = nums.length - 1
+    nums.sort((a, b) => a - b)
+    for (let i = 0; i < nums.length; i++) {
+        if (i != 0 && nums[i] === nums[i - 1]) continue
+        for (let j = i + 1; j < nums.length; j++) {
+            if (j != i + 1 && nums[j] === nums[j - 1]) continue
+            left = j + 1
+            right = nums.length - 1
+            while (left < right) {
+                let sum = nums[i] + nums[j] + nums[left] + nums[right]
+                if (sum < target) left++
+                if (sum > target) right--
+                if (sum === target) {
+                    res.push([nums[i], nums[j], nums[left], nums[right]])
+                    while (left < right && nums[left] === nums[left + 1]) left++
+                    while (left < right && nums[right] === nums[right - 1]) right--
+                    left++
+                    right--
+                }
+            }
+        }
+    }
+    return res
+};
