70a5c3c6cb7f6284f90e8de9fa52a31f963899c1
OhIAmFine <845782625@qq.com>
2020-08-25 18:51:50 +0800

    add 96th

---
+++ 96/96. 不同的二叉搜索树.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTrees = function (n) {
+    const G = new Array(n + 1).fill(0)
+    G[0] = 1
+    G[1] = 1
+    for (let i = 2; i <= n; i++) {
+        for (let j = 1; j <= i; j++) {
+            G[i] += G[j - 1] * G[i - j]
+        }
+    }
+    return G[n]
+};
