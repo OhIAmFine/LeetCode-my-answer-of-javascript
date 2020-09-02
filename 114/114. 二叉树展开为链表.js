bfcfa33c62b94df9a589ba1775ebb72196be755d
OhIAmFine <845782625@qq.com>
2020-09-02 18:24:59 +0800

    refactor 114th

--- 114/114. 二叉树展开为链表.js
+++ 114/114. 二叉树展开为链表.js
@@ -10,23 +10,43 @@
  * @param {TreeNode} root
  * @return {void} Do not return anything, modify root in-place instead.
  */
-const flatten = (root) => {
-  const helper = (root) => {       // 将当前子树转成一个单链表
-    if (root == null) return null // 遍历到null节点 返回null节点
-    if (root.right) {              // 先生成右子树的单链表
-      helper(root.right)
+var flatten = function (root) {
+    function helper(node) {
+        if (!node) return null
+        if (!node.left && !node.right) return node
+        const left = helper(node.left)
+        const right = helper(node.right)
+        if (left) {
+            node.right = left
+            node.left = null
+            let temp = node.right
+            while (temp.right) {
+                temp = temp.right
+            }
+            temp.right = right
+        } else {
+            node.right = right
+            node.left = null
+        }
+        return node
     }
-    if (root.left) {               // 如果有左子树，生成单链表然后搬运过去
-      const leftFirst = helper(root.left) // 生成单链表，并获取头结点
-      let leftLast = leftFirst    // leftEnd是单链表的尾节点
-      while (leftLast.right) {     // 一直找右节点，获取到单链表的尾节点
-        leftLast = leftLast.right
-      }
-      leftLast.right = root.right // 尾节点后面接左子树展平后的单链表
-      root.right = leftFirst      // 根节点的right改成leftFirst
-      root.left = null            // root.left置为null
+
+    helper(root)
+};
+
+var flatten = function (root) {
+    let curr = root;
+    while (curr !== null) {
+        if (curr.left !== null) {
+            const next = curr.left;
+            let predecessor = next;
+            while (predecessor.right !== null) {
+                predecessor = predecessor.right;
+            }
+            predecessor.right = curr.right;
+            curr.left = null;
+            curr.right = next;
+        }
+        curr = curr.right;
     }
-    return root // 返回出当前子树转成的单链表
-  }
-  helper(root)  // 原地修改，不用返回
-}
+};
