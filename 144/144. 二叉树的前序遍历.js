4d56eb9f95a8bfa3dc5c0adeeaf17b4836a490e6
OhIAmFine <845782625@qq.com>
2020-09-02 18:39:04 +0800

    add  144th

---
+++ 144/144. 二叉树的前序遍历.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root) {
+    if (!root) return []
+    const res = []
+    const stack = [root]
+    while (stack.length) {
+        const cur = stack.pop()
+        res.push(cur.val)
+        if (cur.right) {
+            stack.push(cur.right)
+        }
+        if (cur.left) {
+            stack.push(cur.left)
+        }
+    }
+    return res
+};
+
+var preorderTraversal = function (root) {
+    const res = []
+
+    function dfs(node) {
+        if (!node) return null
+        const val = node.val
+        res.push(val)
+        dfs(node.left)
+        dfs(node.right)
+    }
+
+    dfs(root)
+    return res
+};
