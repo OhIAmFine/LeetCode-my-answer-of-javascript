3c63231399f2801055e40670ccacb324ea0fe3bc
OhIAmFine <845782625@qq.com>
2020-09-26 21:34:59 +0800

    add  515th

---
+++ 515/515. 在每个树行中找最大值.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var largestValues = function (root) {
+  const res = []
+  if (!root) return res
+  const store = [root]
+  let max = -Infinity
+  while (store.length) {
+    const size = store.length
+    for (let i = 0; i < size; i++) {
+      const temp = store.pop()
+      const val = temp.val
+      if (val > max) max = val
+      if (temp.left) store.unshift(temp.left)
+      if (temp.right) store.unshift(temp.right)
+    }
+    res.push(max)
+    max = -Infinity
+  }
+  return res
+}
