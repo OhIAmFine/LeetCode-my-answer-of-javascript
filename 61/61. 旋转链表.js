2ba0d14e0ad7d570cdcb52a8b460058dcafb3c76
OhIAmFine <845782625@qq.com>
2020-10-30 23:55:53 +0800

    add 61th

---
+++ 61/61. 旋转链表.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var rotateRight = function (head, k) {
+    if (!head || !head.next) return head
+    let len = 1
+    let oldTail = head
+    while (oldTail.next) {
+        len++
+        oldTail = oldTail.next
+    }
+    oldTail.next = head
+
+    let pos = len - k % len - 1
+    let newTail = head
+    for (let i = 0; i < pos; i++) newTail = newTail.next
+    let newHead = newTail.next
+    newTail.next = null
+    return newHead
+};
