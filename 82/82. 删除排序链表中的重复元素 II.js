793134c6e88fd04e1197754a99bf14e6a3ffd410
OhIAmFine <845782625@qq.com>
2020-12-04 18:55:50 +0800

    add  82th

---
+++ 82/82. 删除排序链表中的重复元素 II.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+    if (!head || !head.next) return head
+    const eNode = new ListNode(null)
+    eNode.next = head
+
+    let repeat = false
+    let prev = eNode
+    let cur = eNode.next
+    while (cur.next) {
+        console.log(cur.val, prev.val, repeat)
+        if (cur.val !== cur.next.val) {
+            if (repeat) {
+                prev.next = cur.next
+                repeat = false
+            } else {
+                prev = cur
+            }
+        } else {
+            repeat = true
+        }
+        cur = cur.next
+    }
+    if (repeat) {
+        prev.next = cur.next
+    }
+    return eNode.next
+};
