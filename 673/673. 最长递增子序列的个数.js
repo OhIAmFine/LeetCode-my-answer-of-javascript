3a031db8c2b260b8f6ac933eb6181d6918ff9702
OhIAmFine <845782625@qq.com>
2021-02-01 18:35:07 +0800

    add 673th

---
+++ 673/673. 最长递增子序列的个数.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNumberOfLIS = function (nums) {
+    const dp = new Array(nums.length).fill(1)
+    const count = new Array(nums.length).fill(1)
+    for (let i = 1; i < nums.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[i] > nums[j]) {
+                if (dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1
+                    count[i] = count[j]
+                } else if (dp[j] + 1 === dp[i]) {
+                    count[i] += count[j]
+                }
+            }
+        }
+    }
+    let max = Math.max(...dp)
+    let res = 0
+    for (let i = 0; i < count.length; i++) {
+        if (dp[i] === max) res += count[i]
+    }
+    console.log(dp, res, count)
+    return res
+};
