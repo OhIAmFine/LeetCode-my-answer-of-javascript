64bb828c55f5ee1d02df9f9fc170dd40b862be09
OhIAmFine <845782625@qq.com>
2021-02-05 13:32:26 +0800

    add 198th

--- 198/198. 打家劫舍.js
+++ 198/198. 打家劫舍.js
@@ -4,11 +4,9 @@
  */
 var rob = function (nums) {
   if (!nums.length) return 0
-  const res = []
-  res[0] = nums[0]
-  res[1] = Math.max(res[0], nums[1])
-  for (let i = 2; i < nums.length; i++) {
-    res[i] = Math.max(res[i - 1], nums[i] + res[i - 2])
-  }
-  return res[nums.length - 1]
-}
+  const dp = []
+  dp[0] = nums[0]
+  dp[1] = Math.max(dp[0], nums[1])
+  for (let i = 2; i < nums.length; i++)  dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i])
+  return dp[nums.length - 1]
+};
