4486c510e642a4bb8227a307114c9b9059f63540
sam.chen <sam.chen@axinan.com>
2021-03-31 18:52:59 +0800

    refactor 3th

--- 3/3. 无重复字符的最长子串.js
+++ 3/3. 无重复字符的最长子串.js
@@ -2,19 +2,21 @@
  * @param {string} s
  * @return {number}
  */
+/**
+ * @param {string} s
+ * @return {number}
+ */
 var lengthOfLongestSubstring = function (s) {
     let store = new Set()
     let res = 0
-    let left = -1
+    let left = 0
     for (let i = 0; i < s.length; i++) {
-        if (i != 0) {
-            store.delete(s[i - 1])
-        }
-        while (left + 1 < s.length && !store.has(s[left + 1])) {
-            store.add(s[left + 1])
+        if (i != 0)  store.delete(s[i - 1])
+        while (left < s.length && !store.has(s[left])) {
+            store.add(s[left])
             left++
         }
-        res = Math.max(res, left - i + 1)
+        res = Math.max(res, left - i)
     }
     return res
 };
