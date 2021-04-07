ae4cd4f61286a02b6f6c824225b012f4e5bcf04d
sam.chen <845782625@qq.com>
2021-04-07 11:52:34 +0800

    add 153th

--- 125/125. 验证回文串.js
+++ 125/125. 验证回文串.js
@@ -1,53 +1,16 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
 var isPalindrome = function (s) {
-  // if(!s) return true
-  // let str = ''
-  // const reg = /[a-zA-Z0-9]/
-  // for(let i = 0; i < s.length; i ++) {
-  //   if(reg.test(s[i])) {
-  //     str += s[i]
-  //   }
-  // }
-  // console.log(str.toLocaleLowerCase())
-  // const reveredStr = str.toLocaleLowerCase().split('').reverse().join('')
-  // return str.toLocaleLowerCase() === reveredStr
-  // const a = [1,2,3,4,5]
-  // const b = []
-  // try{
-  //   a.forEach((val,idx) => {
-  //     if(val === 3) throw new Error('aaa')
-  //     b.push(val)
-  //   })
-  // }catch (e) {
-  //
-  // }
-  // console.log(b)
-  class Quene {
-    constructor (size) {
-      this.max = size || 10
-      this.quene = []
-      this.promiseQuene = []
-    }
-
-    sort () {
-      this.quene.sort((a, b) => a.level - b.level)
-    }
-
-    add ({ level, func }) {
-      this.quene.push({ level, func })
-      this.sort()
-    }
-
-    async run () {
-      let results = []
-      for (let i = 0; i < this.max; i++) {
-        const { func } = this.quene[i]
-        this.promiseQuene.push(func )
-        results = await Promise.all(this.promiseQuene)
-        this.promiseQuene = []
-        return results
-      }
+  if (!s) return true
+  let str = ''
+  const reg = /[a-zA-Z0-9]/
+  for (let i = 0; i < s.length; i++) {
+    if (reg.test(s[i])) {
+      str += s[i]
     }
   }
+  const reveredStr = str.toLocaleLowerCase().split('').reverse().join('')
+  return str.toLocaleLowerCase() === reveredStr
 }
-
-console.log(isPalindrome('OP'))
--- 142/142. 环形链表 II.js
+++ 142/142. 环形链表 II.js
@@ -38,3 +38,4 @@ var detectCycle = function (head) {
         }
     }
     return null
+}
