d4460bad63fe72ec49347c6836598baad156b7fe
sam.chen <845782625@qq.com>
2021-05-28 18:07:49 +0800

    add offer 32th

--- 剑指 Offer 32 - II. 从上到下打印二叉树 II/剑指 Offer 32 - II. 从上到下打印二叉树 II.js
+++ 剑指 Offer 32 - II. 从上到下打印二叉树 II/剑指 Offer 32 - II. 从上到下打印二叉树 II.js
@@ -9,20 +9,26 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
-var levelOrder = function (root) {
+var levelOrder = function(root) {
+  /*
+      BFS 构造队列 + 判断当前层奇偶调换顺序
+  */
   if (!root) return []
-  const res = []
-  const stack = [root]
-  while (stack.length) {
-    const temp = []
-    const size = stack.length
-    for (let i = 0; i < size; i++) {
-      const node = stack.shift()
-      temp.push(node.val)
-      if (node.left) stack.push(node.left)
-      if (node.right) stack.push(node.right)
-    }
-    res.push(temp)
+
+  const queue = [[root, 0]], res = []
+
+  while (queue.length) {
+    const [node, lev] = queue.shift()
+
+    // 初始化当前层
+    if (!res[lev]) res[lev] = []
+
+    // 奇数层 逆序 1 3 5 偶数层 正序 0 2 4
+    lev & 1 ? res[lev].unshift(node.val) : res[lev].push(node.val)
+
+    node.left && queue.push([node.left, lev + 1])
+    node.right && queue.push([node.right, lev + 1])
   }
+
   return res
-}
+};
